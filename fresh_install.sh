9a0f3161fa58ba7c7b6a6b8f8d50d30bae8aeae3
Sándor Irházi <wuxort@gmail.com>
2015-04-15 22:25:02 +0000

    fresh_install.sh: Fixed initial commit.

--- fresh_install.sh
+++ fresh_install.sh
@@ -2,7 +2,6 @@
 # Run this as root
 
 apt-get -y install \
-# Basic utilities
       apt-show-versions \
       arandr \
       cifs-utils \
@@ -19,10 +18,8 @@ apt-get -y install \
       unzip \
       vim \
       wget \
-# Media
       audacious \
       mplayer \
-# Development
       colorgcc \
       gcc \
       g++ \
@@ -33,11 +30,11 @@ apt-get -y install \
 # Other progz without apt
 #   Chrome
       wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-      apt-get -y install libxss1 libappindicator1 libindicator7
+      apt-get -y install libxss1 libappindicator1 libindicator7 libcurl3
       dpkg -i google-chrome-stable_current_amd64.deb
       rm -f google-chrome-stable_current_amd64.deb
 
 #   Docker
-      apt-get install lxc
+      apt-get -y install lxc
       wget https://get.docker.io/builds/Linux/x86_64/docker-latest -O /usr/local/bin/docker
       chmod 755 /usr/local/bin/docker
